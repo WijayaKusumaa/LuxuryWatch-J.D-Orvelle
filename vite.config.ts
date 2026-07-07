e58bbacbed7276464948e174ec7dd50fb5587612
AkhdarhWijaya <120603181+AkhdarhWijaya@users.noreply.github.com>
2026-07-08 03:29:25 +0700

    Update repository name to LuxuryWatch-J.D-Orvelle

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/LuxuryWatch-Wijaya/', // Dikembalikan ke repository asal Anda
+  base: '/LuxuryWatch-J.D-Orvelle/', // Konfigurasi base path baru Anda (tanpa tanda petik satu karena dilarang GitHub)
 })
