7e56b3c4be73952ad4abf93e7da33088a277b353
AkhdarhWijaya <120603181+AkhdarhWijaya@users.noreply.github.com>
2026-07-08 03:26:55 +0700

    Revert build configs to original repo name

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/luxuryWatch-J.D-Orvelle/', // Diperbarui untuk repositori baru Anda
+  base: '/LuxuryWatch-Wijaya/', // Dikembalikan ke repository asal Anda
 })
